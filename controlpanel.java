d18d7fb18389d96e155c8aad6bf7117552777d75
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-02-07 19:06:10 +0600

    Control features of general remote added

--- controlpanel.java
+++ controlpanel.java
@@ -2,13 +2,22 @@ public class controlpanel {
   public static void main(String args[])
   {
     TV_interface smarttv = new smartTV();
+    TV_interface generaltv = new generalTV();
     smartRemote smartremote = new smartRemote(smarttv);
-
+    generalremote generalRemote = new generalremote(generaltv);
     smartremote.togglePower();
     smartremote.volumeUp();
     smartremote.volumeDown();
     smartremote.channelDown();
     smartremote.channelUp();
     smartremote.showYoutube();
+
+    generalRemote.togglePower();
+    generalRemote.volumeUp();
+    generalRemote.volumeDown();
+    generalRemote.channelDown();
+    generalRemote.channelUp();
+
+
   }
 }
