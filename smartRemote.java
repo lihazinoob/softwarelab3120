b2f52f8277d951a6631f50f41bb30b67af5af06a
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-02-07 18:58:37 +0600

    showYoutube feature added

--- smartRemote.java
+++ smartRemote.java
@@ -1,6 +1,12 @@
 public class smartRemote extends remote {
+  public smartRemote(TV_interface tv)
+  {
+    super();
+  }
   public void showYoutube()
   {
+    smartTV smarttv = new smartTV();
+    smarttv.youTube();
 
   }
 }
